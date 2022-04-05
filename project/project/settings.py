d036321affded379ba21cba565ce73f74ed5610a
mblosiu@interia.pl <mblosiu@interia.pl>
2022-04-05 10:47:48 +0200

    postgresql database settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoblog',
+        'USER': 'postgres',
+        'PASSWORD': 'w1s2a3d4',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
